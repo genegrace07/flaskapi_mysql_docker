7162f00da545d954aafe53b5ab05067e548b6999
mark anthony dador <dador.markanthony@gmail.com>
2026-02-13 22:14:53 +0800

    update for render

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask  import Flask,jsonify,request
 from appdb import Users,Usersdb,Action
+import os
 
 app = Flask(__name__)
 
@@ -57,4 +58,5 @@ def delete_user():
     return jsonify({'message':'id not found'}),404
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=5000)
+    port = int(os.environ.get("PORT",5000))
+    app.run(debug=False,host='0.0.0.0',port=port)
